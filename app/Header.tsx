b867c95f0d2feac1ed44352ff68ec0e34723de5f
Philip Bendiksen <philipbendiksen@gmail.com>
2024-10-22 13:08:51 +0200

    fixed some styling

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,25 +1,48 @@
+"use client";
+
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 const Header = () => {
+  const segment = useSelectedLayoutSegment(); // Hämta aktuell layout-segment
+
   return (
     <header className="bg-primary md:h-20 p-2 flex flex-col md:flex-row text-center justify-between items-center mx-auto rounded-lg shadow-lg">
       {/* Rubrik */}
       <Link href="/">
-        <h1 className="md:ml-12 text-2xl cursor-pointer mb-0">
+        <h1 className="md:ml-12 text-2xl md:text-4xl cursor-pointer mb-3 md:mb-0 mt-2">
           FalkenbergsTouren
         </h1>
       </Link>
 
       {/* Menyn */}
-      <ul className="flex justify-center space-x-12 md:mr-16">
+      <ul className="flex justify-center space-x-12 md:mr-16 mb-1">
         <Link href="/resultat">
-          <li className="cursor-pointer">Resultat</li>
+          <li
+            className={`cursor-pointer ${
+              segment === "resultat" ? "border-b-2 border-black" : ""
+            }`}
+          >
+            Resultat
+          </li>
         </Link>
         <Link href="/info">
-          <li className="cursor-pointer">Info</li>
+          <li
+            className={`cursor-pointer ${
+              segment === "info" ? "border-b-2 border-black" : ""
+            }`}
+          >
+            Info
+          </li>
         </Link>
         <Link href="/bilder">
-          <li className="cursor-pointer">Bilder</li>
+          <li
+            className={`cursor-pointer ${
+              segment === "bilder" ? "border-b-2 border-black" : ""
+            }`}
+          >
+            Bilder
+          </li>
         </Link>
       </ul>
     </header>
