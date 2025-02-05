a8464f2cee73ebaa2ea8c48a7a5465f03adf30c4
Philip Bendiksen <philipbendiksen@gmail.com>
2025-02-05 09:47:09 +0100

    updated info page

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
 const Info = () => {
   return (
-    <div className="text-3xl text-center mt-10">
-      Här är lite mer information
-    </div>
+    <>
+      <div className="text-3xl text-center mt-8">
+        Information om Tour de Falkenberg 2025
+      </div>
+
+      <div className="m-14">
+        <Image
+          src="/images/Reklamblad.jpg"
+          alt="Bada på Skrea Strand"
+          width={800}
+          height={600}
+          className="w-full mt-6 md:w-2/4 m-auto h-auto rounded-lg shadow-lg object-cover object-center"
+        />
+      </div>
+    </>
   );
 };
 
