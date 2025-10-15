654689dbbdb9943f901b2c542dfb34a246da3568
Philip Bendiksen <philipbendiksen@gmail.com>
2025-10-15 17:18:03 +0200

    Added sponsor commented out

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -9,7 +9,7 @@ export default function sponspors() {
       <p className="text-base mb-10 md:text-lg text-center text-gray-700">
         Nedan presenteras våra sponsorer som är med och gör denna tour möjlig
       </p>
-      <section className="grid grid-cols-1 md:grid-cols-3 gap-0 justify-items-center">
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-items-center">
         <div className="flex flex-col items-center transition-transform duration-300 ease-in-out hover:scale-105">
           <Link
             href="https://jjsdack.se/"
@@ -21,7 +21,7 @@ export default function sponspors() {
               alt="Allstars"
               width={250} // Ange bildens bredd
               height={250} // Ange bildens höjd
-              className="rounded-lg max-h-64 min-h-64"
+              className="rounded-lg max-h-64 max-w-52"
             />
           </Link>
         </div>
@@ -32,10 +32,21 @@ export default function sponspors() {
               alt="Allstars"
               width={200} // Ange bildens bredd
               height={200} // Ange bildens höjd
-              className="rounded-lg max-h-64 min-h-64"
+              className="rounded-lg max-h-64 max-w-52"
             />
           </Link>
         </div>
+        {/* <div className="flex flex-col items-center transition-transform duration-300 ease-in-out hover:scale-105">
+          <Link href="https://www.vinbergslantmanna.se/ ">
+            <Image
+              src="/images/Vinbergs-Lantmanna_300x300.jpg"
+              alt="Allstars"
+              width={200} // Ange bildens bredd
+              height={200} // Ange bildens höjd
+              className="rounded-lg max-h-64 max-w-52"
+            />
+          </Link>
+        </div> */}
       </section>
     </div>
   );
